fc3e4ae62cd4d9229d7285c50ac55e670a821a98
anjanireddy2307 <saddianjanireddy@gmail.com>
2021-01-29 17:40:29 +0530

    adding task 2 principles

---
+++ MavenCalculator/src/main/java/com/anjani/epam/MavenCalculator/kissPrinciples.java
@@ -0,0 +1,27 @@
+package com.anjani.epam.MavenCalculator;
+
+ class kissPrinciples {
+
+	public static void main(String[] args) {
+	    try {
+			System.out.println(weekday(1));
+			}catch(Exception e)
+		     {
+				System.out.println(e.getMessage());
+		     }
+	}
+	private static String weekday(int day) throws Exception{
+		if((day<1)||(day>7)) throw new Exception("day must be range 1 to 7");
+		String[] days= {
+				"monday",
+				"tuesday",
+				"wednesday",
+				"thursday",
+				"friday",
+				"saturday",
+				"sunday"
+		};
+		return days[day-1];
+	}
+
+}
